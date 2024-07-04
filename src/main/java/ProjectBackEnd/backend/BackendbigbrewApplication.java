dcdb4f2bbdd823d63be771d757b7abae1ab275cc
kolnieer <tolentinoannenicol@gmail.com>
2024-07-04 13:30:20 +0800

    initial

---
+++ src/main/java/ProjectBackEnd/backend/BackendbigbrewApplication.java
@@ -0,0 +1,13 @@
+package ProjectBackEnd.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendbigbrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendbigbrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjectBackEnd/backend/BackendbigbrewApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectBackEnd.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbigbrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
